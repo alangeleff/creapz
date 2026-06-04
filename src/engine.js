1aa0cfc18c9f132cc7d06a13b721353e6f73a744
Alan Geleff <alan.geleff@gmail.com>
2026-06-04 20:45:19 +0000

    cReaper run: new smoother sheet (frames 2-24 @16fps) + surgical collar-blob removal (grey chunky comps in the below-skull pocket only; fist shading protected by erosion test)

--- src/engine.js
+++ src/engine.js
@@ -1,4 +1,4 @@
-const ASSET_VER='1780605087';
+const ASSET_VER='1780605861';
 async function loadSprites(){
   if (window.SPRITES_INLINE) return window.SPRITES_INLINE;
   const S = await (await fetch('./assets/sprites.json?v='+ASSET_VER)).json();
