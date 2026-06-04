efe2752ed4faf0e011cb1c9003eba13483f31bd3
Alan Geleff <alan.geleff@gmail.com>
2026-06-04 15:10:01 +0000

    Dingbat idle: new cleaner sheet; chroma-keyed junk removal (#969694/#4d4d4a as neutral greys), earring repainted #847a73, pinholes healed

--- src/engine.js
+++ src/engine.js
@@ -1,4 +1,4 @@
-const ASSET_VER='1780584257';
+const ASSET_VER='1780585765';
 async function loadSprites(){
   if (window.SPRITES_INLINE) return window.SPRITES_INLINE;
   const S = await (await fetch('./assets/sprites.json?v='+ASSET_VER)).json();
