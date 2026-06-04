edd1f45c9b2d65dffeb918f924f7e02d99882903
Alan Geleff <alan.geleff@gmail.com>
2026-06-04 04:06:28 +0000

    Brain Dead faces his victims (remove incorrect mirror)

--- src/engine.js
+++ src/engine.js
@@ -1,4 +1,4 @@
-const ASSET_VER='1780544952';
+const ASSET_VER='1780545984';
 async function loadSprites(){
   if (window.SPRITES_INLINE) return window.SPRITES_INLINE;
   const S = await (await fetch('./assets/sprites.json?v='+ASSET_VER)).json();
